3fec519e7e332cf0117d131c6ecd35e7cd5ad3bb
Eric Madsen <superspades@gmail.com>
2017-04-09 20:25:03 -0400

    Initial check in of sales tool.

---
+++ src/gitTutorial/SalesData.java
@@ -0,0 +1,10 @@
+package gitTutorial;
+
+public class SalesData {
+   int data[] = {0, 4, 42};
+
+   public void display() {
+       System.out.println("TODO: Implement data display");
+   } //End display
+
+}  //End class SalesData 
